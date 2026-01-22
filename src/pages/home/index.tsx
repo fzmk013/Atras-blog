acc1ce3329ed1f3ff270926dea53f9f0f9d6ea68
Your Name <fzmk013@gmail.com>
2026-01-22 11:51:26 +0330

    first commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { motion } from "framer-motion";
+
+type Props = {}
+
+function index({}: Props) {
+  return (
+    <motion.div>oldfjk</motion.div>
+  )
+}
+
+export default index
