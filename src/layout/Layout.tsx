975476680f9c093d9b09b61e2391385b747f6ae8
Your Name <fzmk013@gmail.com>
2026-01-24 11:51:16 +0330

    modified layout

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -6,7 +6,7 @@ import HeaderSection from "../sections/header/HeaderSection";
 function Layout({children} : any) {
   return (
     <StyledLayout dir="rtl">
-        {/* <HeaderSection/> */}
+        <HeaderSection/>
         {children}
     </StyledLayout>
   )
@@ -21,22 +21,28 @@ background-image: url(${patterns});
 background-color: #212121;
 /* background: radial-gradient(258.19% 74.24% at 50% 12.21%, #E9F5FC 0%, rgba(203, 220, 243, 0.87) 22%, rgba(5, 52, 186, 0) 100%), radial-gradient(63.78% 39.75% at 50% 18.41%, #29ba05 0%, rgba(5, 52, 186, 0) 100%), radial-gradient(100% 100% at 50% 0%, #29ba05  0%, rgba(5, 52, 186, 0) 100%) black; */
 background-repeat: no-repeat;
-background-size: auto;
-background-position: top left;
-
-
+/* background-size: contain; */
+background-size: cover;
+ /* background-position: center; */
   display: flex;
   flex-direction: column;
 
   align-items: center;
   text-align: center;
   margin: auto;
-
+/* 
 
   min-height: 100vh;
 background-image: url(${patterns});
-/* background-repeat: repeat; */
+background-repeat: repeat;
 background-size: auto;
 background-position: top left;
-background-color: #212121;
+background-color: #212121; */
+
+ /* min-height: 100vh;
+  background-image: url(${patterns});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  background-color: #212121; */
   `
